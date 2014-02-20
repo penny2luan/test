14fcc0fd598de4206b37e00d249e63670cffad05
157379 <157379@HSS-101-09.westwindsor.k12.nj.us>
2014-02-20 10:32:57 -0500

    kingcrab

--- project2_1/src/p18_6/FindLargest.java
+++ project2_1/src/p18_6/FindLargest.java
@@ -5,20 +5,7 @@ public class FindLargest
         public static void main(String[] args)
         {
                 int[] array = {4,1,7,1,0,9,7};
-                System.out.println(findLargest(array));
         }
-        
-        public static int findLargest(int[] a)
-        {
-        	if(a.length == 1)
-        		return a[0];
-        	int[] array = ConstrainedCopy(a, 0, array, 0, a.length-1);
-        	if(findLargest(array) > a[a.length-1])
-        		return findLargest(a);
-        	else
-        		return a[a.length-1];
-        }
-        //not complete
         
         private static int findLargest(int[] a, int start)
         {
