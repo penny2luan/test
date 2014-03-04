dc0e61865107e162ba7f801eebe166f8e41bb2b5
157379 <157379@HSS-101-09.westwindsor.k12.nj.us>
2014-03-04 12:06:18 -0500

    Selection

--- School/sorts/SelectionSort.java
+++ School/sorts/SelectionSort.java
@@ -22,6 +22,17 @@ public class SelectionSort
     	sort(x, index + 1);
     }
     
+    private static int indexOfMin(int[] x, int start)
+    {
+        if(start == x.length - 1)
+        	return start;
+        
+        if(x[indexOfMin(x, start + 1)] < x[start])
+        	return indexOfMin(x, start + 1);
+        else
+        	return start;
+    }
+    
     private static void swap(int[] x, int i, int j)
     {
     	int temp = x[j];
@@ -38,9 +49,12 @@ public class SelectionSort
     	arr[3] = 2;
     	arr[4] = 1;
     	arr[5] = 0;
+    	System.out.println(indexOfMin(arr, 0));
     	sort(arr);
     	for(int x : arr)
     		System.out.print(x + " ");
+    	System.out.println("");
+    	System.out.println(indexOfMin(arr, 0));
     }
     
 
