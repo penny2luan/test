026db92f80936dc34e796ba504c324101ca83dc0
157379 <157379@HSS-101-09.westwindsor.k12.nj.us>
2014-03-07 08:57:18 -0500

    InsertionSort

--- School/sorts/InsertionSort.java
+++ School/sorts/InsertionSort.java
@@ -8,41 +8,30 @@ public class InsertionSort
 	{
 		int[] x = new int[5];
 		x[0] = 5;
-		x[1] = 4;
-		x[2] = 3;
-		x[3] = 2;
+		x[1] = 3;
+		x[2] = 8;
+		x[3] = 6;
 		x[4] = 1;
 		for(int i : x)
 			System.out.print(i + " ");
+		System.out.println("");
 		sort(x);
-		for(int i : x)
-			System.out.print(i + " ");
+		for(int j : x)
+			System.out.print(j + " ");
 	}
-	
-//	public static <T> void sort(T[] x, Comparator<T> c)
-//	{
-//		for(int i = 1; i < x.length; i++)
-//		{
-//			T temp = x[i];
-//			for(int j = i - 1; j >= 0; j--)
-//			{
-//				if(c.compare(temp, x[j]) < 0)
-//					x[j] = temp;
-//			}
-//		}
-//	}
-	
 	
 	public static void sort(int[] x)
 	{
 		for(int i = 0; i < x.length; i++)
 		{
 			int temp = x[i];
-			for(int j = i - 1; j >= 0; j--)
+			int j = i - 1;
+			while(j >= 0 && temp < x[j])
 			{
-				if(x[j] < x[i])
-					x[j] = x[j+1];
+				x[j+1] = x[j];
+				j--;
 			}
+			x[j+1] = temp;
 		}
 	}
 	
