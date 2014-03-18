b615dfbe80b8d33d128bb089a95e4b2d14d44672
157379 <157379@HSS-101-09.westwindsor.k12.nj.us>
2014-03-18 08:36:38 -0400

    MergeSort

--- sort/sorts/MergeSort.java
+++ sort/sorts/MergeSort.java
@@ -1,22 +1,42 @@
 package sorts;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-
 public class MergeSort
 {
-	public static <T> void sort(T[] x, Comparator<T> c)
+	public static void sort(int[] a)
 	{
 		
 	}
 	
-	private static <T> void sort(T[] x, Comparator<T> c, int low, int high)
+	private static void sort(int[] a, int low, int high)
 	{
-		
+	    // check if low is smaller then high, if not then the array is sorted
+	    if (low < high) {
+	      // Get the index of the element which is in the middle
+	      int middle = low + (high - low) / 2;
+	      // Sort the left side of the array
+	      sort(a, low, middle);
+	      // Sort the right side of the array
+	      sort(a, middle + 1, high);
+	      // Combine them both
+	      merge(a, low, middle, high);
+	    }
 	}
-	
-	private static <T> void merge(T[] x, Comparator<T> c, int low, int mid, int high)
+
+	private static void merge(int[] a, int low, int mid, int high)
 	{
-		
-	}
+		  int[] b = new int[mid + 1 - low];
+		  System.arraycopy(a, low, b, 0, b.length);
+
+		  int aLowerIndex = low, bIndex = 0, aHigherIndex = mid + 1;
+		  while (aLowerIndex < aHigherIndex && aHigherIndex <= high)
+		  {
+		    if (a[aHigherIndex] < b[bIndex])
+		      a[aLowerIndex++] = a[aHigherIndex++];
+		    else
+		      a[aLowerIndex++] = b[bIndex++];
+		  }
+
+		  while (aLowerIndex < aHigherIndex)
+		    a[aLowerIndex++] = b[bIndex++];
+		}
 }
