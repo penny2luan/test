2129aea5f038c2077acf1a548512485a69b7c87e
157379 <157379@HSS-101-09.westwindsor.k12.nj.us>
2014-06-09 07:55:28 -0400

    DecoderRing

--- encryption/decryption/DecoderRing.java
+++ encryption/decryption/DecoderRing.java
@@ -1,62 +1,86 @@
 package decryption;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-//BRING YOUR TEXTBOOKS
 public class DecoderRing {
 	
-	private static final int CHAR_TO_INT = 97;
-	private List<Character> knownFreq = new ArrayList<Character>(),
-				foundFreq = new ArrayList<Character>();
+	private static Scanner in;
+	private static final int CHAR_TO_INT = 65;
+	private static int[] foundFreq = new int[26];
+	private static char changeTo, changed;
+	private static String code, decoded, lastState;
 	
-	public DecoderRing()
+	private static void countFrequency(String code)
 	{
-		char[] charsByFrequency = 
-			{'e', 't', 'a', 'o', 'i', 'n', 's', 'h', 'r', 'd', 'l', 'c', 'u',
-				'm', 'w', 'f', 'g', 'y', 'p', 'b', 'v', 'k', 'j', 'x', 'q', 'z'};
-		
-		for(char ch : charsByFrequency)
-			knownFreq.add(ch);
-	}
-	
-	
-	//i = character, derp[i] = frequency
-	public int[] countFrequency(String code)
-	{
-		int[] derp = new int[26];
 		for(int i = 0; i < code.length(); i++)
 		{
-			derp[(int) code.charAt(i) - CHAR_TO_INT]++;
+			if(code.charAt(i) != ' ' && code.charAt(i) != '\'' && code.charAt(i) != '.')
+				foundFreq[(int) code.toUpperCase().charAt(i) - CHAR_TO_INT]++;
 		}
-		return derp;
 	}
 	
-	
-	
-	public String decode(String code)
+	private static void printFrequency(String code)
 	{
-		String decoded = "";
-		int[] frequency = countFrequency(code.toLowerCase());
-		while(foundFreq.size() < knownFreq.size()) {
-			int maxIndex = 0;
-			for(int i=0; i<frequency.length; i++)
-				if(frequency[i] > frequency[maxIndex])
-					maxIndex = i;
-			foundFreq.add((char) (maxIndex + CHAR_TO_INT));
-			frequency[maxIndex] = 0;
+		countFrequency(code);
+		int c = CHAR_TO_INT;
+		for(int i=0; i<foundFreq.length; i++) 
+		{
+			if(foundFreq[i] != 0)
+				System.out.println(((char) c) + " - " + foundFreq[i]);
+			c++;
 		}
-		
-		for(int i=0; i < code.length(); i++) {
-			decoded += knownFreq.get(foundFreq.indexOf(code.toLowerCase().charAt(i)));
-		}
-		return decoded;
 	}
 	
+	public static void replace(char letterToChange, char letterToChangeTo)
+	{
+//		decoded.replace(letterToChange, newChar)
+	}
+	
+	public static void undo()
+	{
+//		replace(changeTo, changed);
+		decoded = decoded.replace(changeTo, changed);
+	}
+	
+		
 	public static void main(String[] args)
 	{
-		DecoderRing dr = new DecoderRing();
-		System.out.println(dr.decode(""));
+		code = "HI MY NAME IS JASON I LIKE PIE AND ALL SORTS OF GOOD'. FOOD AND IM BORED SO IM GOING TO MAKE THIS SUPER LONG WHEEEeeeeeeeeejubguheeeeE";
+		decoded = code;
+		in = new Scanner(System.in);
+		printFrequency(code);
+		
+		
+		boolean done = false;
+		while(!done) {
+			System.out.println("Original | " + code);
+			System.out.println("Working  | " + decoded);
+			System.out.println("Please select action: ");
+			System.out.println("    replace    undo    finish");
+			String input = in.next();
+			switch(input.toLowerCase()) {
+			case "replace":
+				System.out.println("Input character to change: ");
+				changed = in.next().toUpperCase().charAt(0);
+				System.out.println("Input character to change to: ");
+				changeTo = in.next().toUpperCase().charAt(0);
+//				replace(c1, c2);
+				lastState = decoded;
+				decoded = decoded.replace(changed, changeTo);
+				break;
+			case "undo":
+//				undo();
+				if(lastState != null && lastState != decoded)
+					decoded = lastState;
+				else
+					System.out.println("Cannot undo.");
+				break;
+			case "finish":
+				System.out.println("Final result | " + decoded);
+				done = true;
+				break;
+			}
+		}
 	}
 
 }
